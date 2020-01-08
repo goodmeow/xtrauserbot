5abd0c79cc1f41ad73b601aac3d715b173de4462
goodmeow <harunbam3@gmail.com>
2020-01-08 09:29:20 +0700

    Update alive & helper
    - enable search version for python and telethon.
    - remove unwanted strings, credits still on alive message. so dont worry!

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,8 +1,10 @@
 """.admin Plugin for @XtraTgBot"""
 import asyncio
 from telethon import events
+from telethon import version
 from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
+from platform import uname, python_version
+
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
@@ -11,7 +13,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @A_Dark_Princ3\n`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     f"`My peru owner`: {DEFAULTUSER}\n"
-                     "[Deploy this userbot Now](https://github.com/Dark-Princ3/X-tra-Telegram)")
+    await alive.edit("dis @XtraTgBot\n\n"
+                     f"Telethon version: {version.__version__} \n"
+                     f"Python version: {python_version()} \n"
+                     f"`me`: {DEFAULTUSER} \n"
+                     )
